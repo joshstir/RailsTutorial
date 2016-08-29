8f5f90397d584186a6a15d9888e01d407b44c9c8
jstir@osv.com <jstir@osv.com>
2016-08-29 12:59:31 -0400

    fixed test

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -17,7 +17,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     get signup_path
     assert_difference 'User.count', 1 do
       post users_path, { user: { name:  "Example User",
-                                         email: "user@example.com",
+                                         email: "joshostir@gmail.com",
                                          password:              "password",
                                          password_confirmation: "password" } }
     end
