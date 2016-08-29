9d1bab353d818060e6a529a2473b30f7d631d925
Josh <joshostir@gmail.com>
2016-08-28 22:48:54 -0400

    working on login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,16 @@ class SessionsController < ApplicationController
   end
   
   def create
-   render :new
+    debugger
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      #log the user in and redirect to the user's show page
+    else
+      flash.now[:danger] = "Invalid email/password combination" # not quite right
+      render :new
+    end
+  end
+  
+  def destroy
   end
 end
